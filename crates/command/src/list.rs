cd76b2373cf3561a97052c7152d2ee0234c5d758
Robin <robin.tordly@nav.no>
2023-05-08 08:12:52 +0200

    reuse code in recursive list op

--- crates/command/src/list.rs
+++ crates/command/src/list.rs
@@ -1,7 +1,7 @@
 use config::config::Config;
 use config::dependencies::dependency::Dependency;
 use util::buildk_output::BuildkOutput;
-use util::colorize::{Color, Colorize, Colors};
+use util::colorize::{Color, Colors};
 use util::PartialConclusion;
 
 use crate::Command;
@@ -10,32 +10,29 @@ impl Command {
     pub fn list(&self, config: &Config) -> BuildkOutput {
         let mut output = BuildkOutput::default();
 
-        config.manifest.dependencies.iter().for_each(|dependency| {
-            match dependency.is_cached() {
-                true => println!("{}", format!("{:<14}{}", "[cached]", dependency.name).as_gray()),
-                false => println!("{}", format!("{:<14}{}", "[fetched]", dependency.name).as_green()),
-            };
-
-            Self::decend(dependency, 1);
-        });
+        list_dependencies(&config.manifest.dependencies, 0);
 
         output.conclude(PartialConclusion::SUCCESS);
         output
     }
+}
 
-    fn decend(dependency: &Dependency, depth: usize) {
-        let color = Color::get_index(depth);
-        dependency.transitives().iter().for_each(|transitive| {
-            let display = format!(
-                "{:>depth$}{:<14}{}:{}",
-                "",
-                "[transitive]",
-                transitive.name,
-                transitive.version,
-                depth=depth*2,
-            );
-            println!("{}", display.colorize(&color));
-            Self::decend(transitive, depth + 1)
-        });
-    }
+fn list_dependencies(dependencies: &[Dependency], depth: usize) {
+    let color = Color::get_index(depth);
+    dependencies.iter().for_each(|dep| {
+        let status = match dep.is_cached() {
+            true => "[cached]",
+            false => "[missing]",
+        };
+        let display = format!(
+            "{:>depth$}{:<14}{}:{}",
+            "",
+            status,
+            dep.name,
+            dep.version,
+            depth=depth*2,
+        );
+        println!("{}", display.colorize(&color));
+        list_dependencies(&dep.transitives(), depth + 1)
+    })
 }
