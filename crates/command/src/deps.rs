407640e3eda5e485a4a4339c9262d515e56509f1
Robin <robin.tordly@nav.no>
2024-03-11 10:58:23 +0100

    count hit and misses

--- crates/command/src/deps.rs
+++ crates/command/src/deps.rs
@@ -26,43 +26,80 @@ fn termtree_status(dep: &Dependency) -> String {
     }
 }
 
+pub struct Counter {
+    hit: usize,
+    miss: usize,
+}
+
+impl Counter {
+    fn hit() -> Self {
+        Self {
+            hit: 1,
+            miss: 0,
+        }
+    }
+
+    fn miss() -> Self {
+        Self {
+            hit: 0,
+            miss: 1,
+        }
+    }
+
+    fn apply(&mut self, other: Counter) {
+        self.hit += other.hit;
+        self.miss += other.miss;
+    }
+}
+
 pub fn build_termtree(
-    dependency: Dependency,
+    dep: Dependency,
     mut traversed: Vec<Dependency>,
     depth: usize,
     limit: usize,
-) -> anyhow::Result<(Tree<String>, Vec<Dependency>)> {
-    match traversed.contains(&dependency) {
+) -> anyhow::Result<(Tree<String>, Vec<Dependency>, Counter)> {
+    match traversed.contains(&dep) {
         true => anyhow::bail!("already processed"),
-        false => traversed.push(dependency.clone())
+        false => traversed.push(dep.clone())
     }
 
-    let status = termtree_status(&dependency);
-    let display = termtree_display(&status, &dependency);
+    let mut counter_acc = match dep.is_cached() {
+        true => Counter::hit(),
+        false => Counter::miss(),
+    };
+
+    let status = termtree_status(&dep);
+    let display = termtree_display(&status, &dep);
     let color = Color::get_index(depth);
     let label = display.colorize(&color).to_string();
 
     if depth < limit {
-        let (tree, traversed) = dependency
+        let (tree, traversed) = dep
             .transitives()
             .into_iter()
             .filter(|it| !traversed.contains(it))
             .fold(
                 (Tree::new(label), traversed.clone()),
-                |(mut root, root_trav), entry| {
-                    match build_termtree(entry, root_trav.clone(), depth + 1, limit) {
-                        Ok((tree, traversed)) => {
-                            root.push(tree);
-                            (root, traversed)
+                |(mut tree_acc, trav_acc), entry| {
+                    match build_termtree(
+                        entry,
+                        trav_acc.clone(),
+                        depth + 1,
+                        limit,
+                    ) {
+                        Ok((tree, traversed, counter)) => {
+                            tree_acc.push(tree);
+                            counter_acc.apply(counter);
+                            (tree_acc, traversed)
                         }
                         Err(_) => {
-                            (root, root_trav)
+                            (tree_acc, trav_acc)
                         }
                     }
                 });
-        Ok((tree, traversed))
+        Ok((tree, traversed, counter_acc))
     } else {
-        Ok((Tree::new(label), traversed))
+        Ok((Tree::new(label), traversed, counter_acc))
     }
 }
 
@@ -78,15 +115,17 @@ impl<'a> Command for Deps<'a> {
 
         let limit = arg.unwrap_or(999);
 
-        if !manifest.dependencies.is_empty() {
-            println!("{} found   {} missing", "".as_green(), " ".as_red());
+        let mut traversed = vec![];
+        let mut counter_acc = Counter { hit: 0, miss: 0 };
+        for dep in manifest.dependencies.iter() {
+            let (tree, newly_traversed, counter) = build_termtree(dep.clone(), traversed.clone(), 0, limit).unwrap();
+            traversed = newly_traversed;
+            counter_acc.apply(counter);
+            print!("{}", tree);
         }
 
-        let mut traversed = vec![];
-        for dep in manifest.dependencies.iter() {
-            let (tree, newly_traversed) = build_termtree(dep.clone(), traversed.clone(), 0, limit).unwrap();
-            traversed = newly_traversed;
-            print!("{}", tree);
+        if !manifest.dependencies.is_empty() {
+            println!("found {} {}  missing {}{}", "".as_green(), counter_acc.hit, " ".as_red(), counter_acc.miss);
         }
 
         // manifest.dependencies.iter().for_each(|dep| {
@@ -235,9 +274,10 @@ mod tests {
     fn test_termtree() -> anyhow::Result<()> {
         // let dep = Dependency::new(Kind::Source, Name::from("io.ktor.ktor-server-core"), Version::from("2.3.7"))?;
         let dep = Dependency::new(Kind::Source, Name::from("org.jetbrains.kotlin.kotlin-stdlib"), Version::from("1.9.22"))?;
-        let (tree, _) = build_termtree(dep, vec![], 0, 1)?;
+        let (tree, _, counter) = build_termtree(dep, vec![], 0, 1)?;
 
         println!("{tree}");
+        println!("hit: {} miss: {}", counter.hit, counter.miss);
         Ok(())
     }
 }
